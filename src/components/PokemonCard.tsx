8cd282dd9c77394d7dd431b2d8f475f83e236c12
Marc Juvénal <marcjuvenal95@gmail.com>
2024-10-23 15:24:26 +0200

    React basics 3 affiche des données

--- src/components/PokemonCard.tsx
+++ src/components/PokemonCard.tsx
@@ -1,12 +1,42 @@
+const pokemonList = [
+    {
+        name: "bulbasur",
+        imgsrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+        name: "mew"
+    },
+];
+
+
 const PokemonCard = () => {
+    // Modification de la variable pokemon pour utiliser le deuxième Pokémon de pokemonList
+    const pokemon = pokemonList[0]; // Mew
+
     return (
-        <figure>
-            <img src="https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png" alt="Bulbasaur" />
-            <figcaption>
-                Bulbasaur
-            </figcaption>
-        </figure>
+        <div>
+            <h1>{pokemon.name}</h1>
+            {pokemon.imgsrc ? (
+                <img src={pokemon.imgsrc} alt={pokemon.name} />
+            ) : (
+                <p>???</p>
+            )}
+        </div>
     );
+
+
+
+
+// return (
+//   <figure>
+//     <img src={pokemon.imgSrc} alt={pokemon.name} />
+//   <figcaption>
+//     {pokemon.name}
+// </figcaption>
+// </figure>
+// );
+
 }
 
 export default PokemonCard;
